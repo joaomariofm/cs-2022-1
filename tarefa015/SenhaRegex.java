c6aaf99af46cb06e36f7b6f3119e8fb4fb87d16b
Joao Mario <joao.martins@soluti.com.br>
2022-08-12 20:22:04 -0300

    entrega da tarefa 015

---
+++ tarefa015/SenhaRegex.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SenhaRegex {
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Digite a expressão a ser testada: ");  
+    String str = sc.nextLine(); 
+
+    Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{5,30}$", Pattern.UNICODE_CHARACTER_CLASS);
+    Matcher matcher = pattern.matcher(str);
+
+    boolean matchFound = matcher.find();
+    if(matchFound) {
+      System.out.println("Match found");
+    } else {
+      System.out.println("Match not found");
+    }
+  }
+}
